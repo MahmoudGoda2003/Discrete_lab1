9a0316803d037ef99faaaae11c14ffb5f9ce568e
MahmoudGoda2003 <95986689+MahmoudGoda2003@users.noreply.github.com>
2022-10-13 00:31:24 +0200

    start

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "bit_operations.h"
+#include "bits _applications.h"
+
+
+int main() {
+    BitsApplications appliction;
+    cout<<appliction.countOnes(7);
+    return 0;
+}
